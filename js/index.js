7ef687d2ec23ab35b73805fb8a15c9f3a6672a53
ajaybaldaniya <ajaybaldaniya791@gmail.com>
2023-08-24 14:57:49 +0530

    playoza

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+        $('#slider1').owlCarousel({
+            loop:true,
+            items:2,
+            margin:50,
+            nav:true,
+            navText:['<i class="fa-solid fa-arrow-right c-0"></i>','<i class="fa-solid fa-arrow-left c-0"></i>'] ,
+        })
+  });
+
+
+  $('#slider2').owlCarousel({
+    loop:true,
+            items:3,
+            margin:50,
+            nav:true,
+            navText:['<i class="fa-solid fa-arrow-right s2-i"></i>','<i class="fa-solid fa-arrow-left s2-ii"></i>'] ,
+})
+
+$('#slider3').owlCarousel({
+  loop:true,
+          items:4,
+          margin:50,
+          nav:true,
+          navText:['<i class="fa-solid fa-arrow-right s2-i"></i>','<i class="fa-solid fa-arrow-left s2-ii"></i>'] ,
+})
